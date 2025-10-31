0229ac571d1b329c9df874959a8935c0f39db6dd
Nana Gyamfi Prempeh <98756444+NanaGyamfiPrempeh30@users.noreply.github.com>
2025-10-31 11:21:47 +0000

    Root module

---
+++ app-dashboard/infra/terraform/main.tf
@@ -0,0 +1,20 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.location
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = false
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
